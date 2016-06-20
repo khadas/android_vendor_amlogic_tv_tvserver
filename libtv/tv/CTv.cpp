259b740f5390c3da2534e547656ed96b3caed099
Jing Wei <jing.wei@amlogic.com>
2016-06-20 14:23:21 +0800

    PD#126778: add timeout before HDMI 2.0 on/off
    Change-Id: I956871f97d6ec913174e5cbd7110fb94b1a65d9a

--- libtv/tv/CTv.cpp
+++ libtv/tv/CTv.cpp
@@ -5097,6 +5097,7 @@ int CTv::GetHdmiAvHotplugDetectOnoff()
 int CTv::SetHdmiEdidVersion(tv_hdmi_port_id_t port, tv_hdmi_edid_version_t version)
 {
     SetAudioMuteForTv ( CC_AUDIO_MUTE );
+    usleep(500 * 1000);
     Tv_HDMIEDIDFileSelect(port, version);
     SSMSetHDMIEdid(port);
     mHDMIRxManager.HdmiRxEdidUpdate();
